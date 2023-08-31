70af0fbb8a47775b97a1b056de65e0cf07a10278
SojibDas <dasojib49@gmail.com>
2023-09-01 02:13:39 +0600

    qna and user section done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -27,20 +30,72 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     //   await client.connect();
-      
-    const courseCollection = client.db("RunTheStack").collection("courses")
 
-      
-      app.get('/courses', async (req, res) => {
-        const cursor = courseCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    const courseCollection = client.db("RunTheStack").collection("courses")
+    const usersCollection = client.db("RunTheStack").collection("users")
+
+    const questionCollection = client.db('RunTheStack').collection('question')
+
+    // get question data from mongodb
+    app.get('/question', async (req, res) => {
+      const cursor = questionCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post('/question', async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await questionCollection.insertOne(item)
+      res.send(result)
+    })
+
+    //all the user collection
+
+    app.get('/users', async (req, res) => {
+      const cursor = usersCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = {
+        email: email
+      }
+      const options = {
+        upsert: true
+      }
+      const updateDoc = {
+        $set: user,
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+      console.log(result);
+      res.send(result)
+    })
+
+
+    app.get('/courses', async (req, res) => {
+      const cursor = courseCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    //save room in database
+    app.post('/rooms', async (req, res) => {
+      const room = req.body;
+      console.log(room);
+      const result = await roomsCollection.insertOne(room)
+      res.send(result)
     })
 
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({
+      ping: 1
+    });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -54,9 +109,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Stack Server is Running')
+  res.send('Stack Server is Running')
 })
 
 app.listen(port, () => {
-    console.log(`Stack Server is running on port ${port}`);
+  console.log(`Stack Server is running on port ${port}`);
 })
